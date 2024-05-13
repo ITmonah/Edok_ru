01794aa8f6255da867e054bf921b84df517185ba
ITmonah <vasin.sasha26@gmail.com>
2024-05-13 16:45:00 +0500

    add recipe steps btns

--- pages/add_recipe.vue
+++ pages/add_recipe.vue
@@ -66,18 +66,194 @@
           </div>
         </div>
       </div>
-      <div class="recipe_input">
+      <div class="recipe_input steps_recipe">
         <label for="input_add_step">Шаги рецепта</label>
-        <input type="text" placeholder="..." id="input_add_step" />
+        <div v-for="n in steps_count" :key="n">
+          <div
+            v-if="steps_count >= n"
+            style="display: flex; flex-direction: column; gap: 6px"
+          >
+            <div class="recipe_delete_step">
+              <h2>{{ n }} шаг</h2>
+              <button
+                v-if="steps_count == n && steps_count != 1"
+                @click="deleteStep"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="11"
+                  height="11"
+                  viewBox="0 0 11 11"
+                  fill="none"
+                >
+                  <path
+                    d="M1 1L10 10"
+                    stroke="#333333"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M10 1L1 10"
+                    stroke="#333333"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+            </div>
+            <input type="text" placeholder="..." id="input_add_step" />
+            <button v-if="n < 10 && steps_count == n" @click="countStep">
+              +
+            </button>
+          </div>
+        </div>
       </div>
+      <div class="recipe_input">
+        <label for="input_add_time">Время готовки</label>
+        <input
+          type="time"
+          placeholder="..."
+          id="input_add_time"
+          style="width: 144px; font-size: 27px"
+        />
+      </div>
+      <h1 class="add_h">Категория блюда</h1>
+      <div class="recipe_add_category">
+        <div class="recipe_input">
+          <div class="recipe_input_items">
+            <input
+              type="checkbox"
+              name="time_category"
+              id="time_category_breakfast"
+            />
+            <label for="time_category_breakfast">завтрак</label>
+          </div>
+          <div class="recipe_input_items">
+            <input
+              type="checkbox"
+              name="time_category"
+              id="time_category_lunch"
+            />
+            <label for="time_category_lunch">обед</label>
+          </div>
+          <div class="recipe_input_items">
+            <input
+              type="checkbox"
+              name="time_category"
+              id="time_category_diner"
+            />
+            <label for="time_category_diner">ужин</label>
+          </div>
+        </div>
+        <div class="recipe_input">
+          <div class="recipe_input_items">
+            <input type="radio" name="time_category" id="time_category_1" />
+            <label for="time_category_1">десерт</label>
+          </div>
+          <div class="recipe_input_items">
+            <input type="radio" name="time_category" id="time_category_2" />
+            <label for="time_category_2">мясо</label>
+          </div>
+          <div class="recipe_input_items">
+            <input type="radio" name="time_category" id="time_category_3" />
+            <label for="time_category_3">суп</label>
+          </div>
+          <div class="recipe_input_items">
+            <input type="radio" name="time_category" id="time_category_4" />
+            <label for="time_category_4">рыба</label>
+          </div>
+          <div class="recipe_input_items">
+            <input type="radio" name="time_category" id="time_category_5" />
+            <label for="time_category_5">напиток</label>
+          </div>
+          <div class="recipe_input_items">
+            <input type="radio" name="time_category" id="time_category_6" />
+            <label for="time_category_6">основное</label>
+          </div>
+        </div>
+      </div>
+      <div class="recipe_input">
+        <label>Лицевая картинка рецепта</label>
+        <button>выбрать файл</button>
+      </div>
+      <div class="recipe_input">
+        <label>Дополнительные картинки блюда</label>
+        <button>выбрать файл</button>
+      </div>
+      <button class="add_recipe_btn">Добавить рецепт</button>
     </div>
   </div>
 </template>
   
 <script setup>
 </script>
+
+<script>
+export default {
+  data() {
+    return {
+      steps_count: 1,
+    };
+  },
+  methods: {
+    countStep: function () {
+      this.steps_count = this.steps_count + 1;
+    },
+    deleteStep: function () {
+      if (this.steps_count > 1) {
+        this.steps_count = this.steps_count - 1;
+      }
+    },
+  },
+};
+</script>
   
 <style scoped>
+.add_recipe_btn {
+  border-radius: 3px;
+  background: rgb(235, 81, 96);
+  width: 310px;
+  height: 47px;
+  color: rgb(255, 255, 255);
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 24px;
+  margin-top: 10px;
+}
+.recipe_input button {
+  width: 223px;
+  height: 35px;
+  margin-top: 2px;
+  box-sizing: border-box;
+  border: 1px solid rgb(235, 81, 96);
+  background: rgb(255, 255, 255);
+  color: rgb(235, 81, 96);
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+  transition: 0.1s;
+}
+.recipe_input button:hover {
+  background: rgb(247, 247, 247);
+}
+.recipe_input button:active {
+  background: #eb5160;
+  color: #fff;
+}
+.recipe_input_items {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+}
+.recipe_add_category {
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  gap: 28px;
+}
 .ingr_right {
   display: flex;
   flex-direction: column;
@@ -93,6 +269,7 @@
   display: flex;
   flex-direction: column;
   gap: 20px;
+  margin-bottom: 30px;
 }
 .recipe_input select {
   height: 41px;
@@ -121,7 +298,8 @@
   border-radius: 3px;
   padding: 10px;
 }
-.recipe_input label {
+.recipe_input label,
+.add_h {
   color: rgb(0, 0, 0);
   font-size: 24px;
   font-weight: 500;
@@ -160,5 +338,91 @@
   line-height: 20px;
   position: absolute;
   top: -23px;
+}
+.recipe_add_category .recipe_input {
+  gap: 13px;
+}
+.recipe_input_items input[type="checkbox"],
+input[type="radio"] {
+  width: 20px;
+  height: 20px;
+  border: 0;
+  background: rgb(235, 235, 235);
+  appearance: none;
+  cursor: pointer;
+}
+.recipe_input_items input[type="radio"] {
+  border-radius: 45px;
+}
+.recipe_input_items input[type="checkbox"]:checked,
+input[type="radio"]:checked {
+  background-color: #eb5160;
+  position: relative;
+}
+.recipe_input_items input[type="checkbox"]::before,
+input[type="radio"]::before {
+  content: "";
+  position: absolute;
+  top: 3px;
+  left: 7px;
+  width: 7px;
+  height: 12px;
+  border: 3px solid transparent;
+  border-left: none;
+  border-top: none;
+  transform: rotate(45deg) scale(1);
+}
+.recipe_input_items input[type="checkbox"]:checked::before,
+input[type="radio"]:checked::before {
+  border-color: #fff;
+  animation: checkAnim 0.2s ease;
+}
+.recipe_input_items label {
+  color: rgb(0, 0, 0);
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 20px;
+  text-align: left;
+  user-select: none;
+}
+.steps_recipe button {
+  width: 52px;
+  height: 19px;
+  color: #fff;
+  font-weight: 400;
+  line-height: 20px;
+  font-size: 24px;
+  border-radius: 5px;
+  background: rgb(235, 81, 96);
+}
+.steps_recipe button:hover {
+  background: rgb(235, 81, 96);
+  opacity: 0.8;
+  transform: scale(1.05);
+}
+.recipe_delete_step {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.recipe_delete_step button {
+  background: #fff;
+  border: 0;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.1s;
+}
+.recipe_delete_step button:hover {
+  background: #ebebeb;
+  transform: scale(1);
+}
+@keyframes checkAnim {
+  from {
+    transform: rotate(45deg) scale(0);
+  }
 }
 </style>
